e9dc9bd29e865c33816323e0665f63955b36ab1e
barnez299 <barnez29@gmail.com>
2020-07-22 13:56:52 +0200

    using default params in function

---
+++ defaultparms.js
@@ -0,0 +1,8 @@
+// using default parameters within a function
+
+// example: create a food function with a default value
+
+function foodShopping(food = 'something') {
+    console.log(`I am going to buy ${food}from the shop`);
+}
+foodShopping();
